34a4c4af6b6672e680d7f1560a99b55a2f6560be
Sergey Dankov <dankov@it.web2age.com>
2023-03-28 19:48:05 +0300

    user permission

---
+++ app/database/models/user_permission.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Boolean, Column, ForeignKey, String, BigInteger
+from app.kernel.database import Base
+
+class RolesPermission(Base):
+
+    __tablename__ = "roles_permission"
+    id = Column('id', BigInteger, primary_key=True)
+    user_id = Column(ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"))
+    module_id = Column(ForeignKey("permissions.id", ondelete="CASCADE", onupdate="CASCADE")) 
+    is_active = Column(Boolean, default=True)
+    is_available = Column(Boolean, default=True)
+    method_access = Column(String(10), default='00000')
+    
+    def __repr__(self) -> str:
+        return "<UsersPermission("\
+            f"user_id={self.user_id}"\
+            f"module_id={self.module_id}"\
+            f"is_active={self.is_active}"\
+            f"is_available={self.is_available}"\
+            f"method_access={self.method_access}"\
+            ")>" 
+            
