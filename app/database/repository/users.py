d1b276a7413480a6322f58feade927da7e990062
Sergey Dankov <Dankov.SS@lexicom.ai>
2023-04-13 13:05:03 +0300

    bug fix

--- app/database/repository/users.py
+++ app/database/repository/users.py
@@ -95,7 +95,7 @@ class UserRepository(SuperRepository):
                                   StatusModel.name.label("status"), 
                                   PositionModel.name.label("position"),
                                   ImagesModel.path.label("path_image"),
-                                  InnerPhone.name.label("user_inner_phone")
+                                  InnerPhone.phone_number.label("user_inner_phone")
                                   )\
                     .join(ImagesModel, ImagesModel.id == self.base_model.image_id, isouter=True)\
                     .join(StatusModel, StatusModel.id == self.base_model.status_id, isouter=True)\
