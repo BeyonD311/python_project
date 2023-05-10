cc08a69ce94b0ff75ca4b7debf45d4e59eb5dfec
Sergey Dankov <Dankov.SS@lexicom.ai>
2023-05-10 15:50:14 +0300

    update

--- app/http/controllers/user_status_controller.py
+++ app/http/controllers/user_status_controller.py
@@ -6,6 +6,7 @@ from dependency_injector.wiring import Provide, inject
 from app.http.services.users import UserService
 from app.http.services.helpers import default_error
 from app.http.services.logger_default import get_logger
+from app.http.services.sutecrm import send_call_post
 
 log = get_logger("status_controller.log")
 
@@ -57,6 +58,8 @@ async def update_status(
             user_id = decode['azp']
         
         await user_service.set_status(user_id, status_id=status_id, call_id=call_id)
+        if call_id is not None:
+            await send_call_post(call_id, "1003")
         result = {
             "message": "set status",
             "description": f"Новый статус ID={status_id} установлен."
@@ -88,6 +91,8 @@ async def update_status_asterisk(
     try:
         log.debug(f"Input params: status_cod = {status_cod}; uuid = {uuid}; status_time = {status_time}; caller = {caller}")
         await user_service.set_status_by_aster(uuid=uuid, status_code=status_cod, status_time=status_time, incoming_call=caller, call_id=call_id)
+        if call_id is not None:
+            await send_call_post(call_id, "1003")
         result = {
             "message": "set status"
         }
