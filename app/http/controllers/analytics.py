2085a5b5831480878f0aa38d75ac8e4e5073fd2f
Sergey Dankov <Dankov.SS@lexicom.ai>
2023-05-17 10:04:15 +0300

    cretate controller

--- app/http/controllers/analytics.py
+++ app/http/controllers/analytics.py
@@ -4,3 +4,11 @@ from dependency_injector.wiring import Provide, inject
 from app.http.services.helpers import parse_params_num
 from app.database import NotFoundError
 from app.kernel import Container
+
+security = HTTPBearer()
+
+route = APIRouter(
+    prefix="/users/analytics",
+    tags=['status'],
+    responses={404: {"description": "Not found"}} 
+)
