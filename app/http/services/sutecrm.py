168d7b370280dea415962a6ca0f20aa674074462
Sergey Dankov <Dankov.SS@lexicom.ai>
2023-05-24 15:50:19 +0300

    update

--- app/http/services/sutecrm.py
+++ app/http/services/sutecrm.py
@@ -1,9 +1,15 @@
-import aiohttp, json
+import aiohttp
 from os import getenv
-from pydantic import BaseModel
+from enum import Enum
 
 HOST = getenv("SUTECRM_HOST")
 PORT = getenv("SUTECRM_POST")
+
+async def send_contact_post(phone: str) -> int:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{HOST}:{PORT}/api/call", json={"phone":phone}) as res:
+            return res.status
+
 
 async def send_call_post(id: str, phone: str) -> int:
     async with aiohttp.ClientSession() as session:
@@ -14,3 +20,9 @@ async def send_call_patch(id: str, disposition:str, duration: int, direction:str
     async with aiohttp.ClientSession() as session:
         async with session.patch(f"{HOST}:{PORT}/api/call", json={id:id, disposition:disposition, duration: duration, direction:direction}) as res:
             return res.status
+        
+class Events(Enum):
+    PRECALL: send_contact_post
+    CALLWATING: send_contact_post
+    CALLBACK: send_call_post
+    EXTERNALCALL: send_call_post
