111cffbdd7e26bd0fd4c4c0665c9d096d467e89d
lizagrechishckina@yandex.ru <lizagrechishckina@yandex.ru>
2023-05-25 11:13:14 +0300

    add worker deleted old audio dile

--- worker.py
+++ worker.py
@@ -1,7 +1,9 @@
 import logging, asyncio
 from main import create_container
 from app.http.services.ssh import scp_asterisk_conn
-
+import os
+from datetime import datetime, timedelta
+from celery.schedules import crontab  
 from celery import Celery
 
 celery_app = Celery(broker='redis://redis:6379/0')
@@ -46,6 +48,29 @@ async def download_audio_asterisk():
 def download():
     asyncio.run(download_audio_asterisk())
 
+@celery_app.task(name='deleted_old_audio_file')
+def del_audio_file():
+    path = os.getenv('LOCAL_PATH_FILECALL')
+
+    dir_list = os.listdir(path)
+    date_utc= datetime.utcnow()
+    date_1_week = date_utc- timedelta(weeks=1)
+    date_1_week_utc = date_1_week.timestamp()
+
+    if dir_list:
+        old_audio_list = list(filter(lambda name: os.path.getctime(f'{path}/{name}')< date_1_week_utc and name!='.gitignore', dir_list))
+        print(old_audio_list)
+        print(len(old_audio_list))
+
+        for old_audio in old_audio_list:
+            try:
+                os.remove(f'{path}/{old_audio}')
+            except Exception as exception:
+                beat_logger.error(exception, stack_info=True)
+        
+        # ИЛИ
+        # old_audio_list = list(map(lambda name: print('deleted2- ', f'{path}/{old_audio}') if os.path.getctime(f'{path}/{name}')< date_1_week_utc and name!='.gitignore' else '', dir_list))
+  
 
 
 celery_app.conf.beat_schedule = {
@@ -53,11 +78,12 @@ celery_app.conf.beat_schedule = {
         'task': 'check_queue',
         'schedule': 10.0
     },
-}
-
-celery_app.conf.beat_schedule = {
     'add-every-20-seconds': {
         'task': 'download_audio_asterisk',
-        'schedule': 20.0
+        'schedule': 8.0
+    },
+    'execute-daily-at-midnight': {
+        'task': 'deleted_old_audio_file',
+        'schedule': crontab(minute=0, hour=0)
     },
 }
